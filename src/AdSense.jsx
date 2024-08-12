72b89cd9c65818d4a0f09ae9d4428ff03aff94c7
Pritesh Golakiya <priteshgolakiya00@gmail.com>
2024-08-12 11:20:55 +0530

    "Updated script tag in index.html and added error handling in AdSense.jsx"

--- src/AdSense.jsx
+++ src/AdSense.jsx
@@ -2,18 +2,22 @@ import { useEffect } from "react";
 
 const AdSense = () => {
   useEffect(() => {
-    (window.adsbygoogle = window.adsbygoogle || []).push({});
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (error) {
+      console.log("error: ", error);
+    }
   }, []);
 
   return (
     <ins
       className="adsbygoogle"
-      style={{ display: "block" }}
+      style="display:block"
       data-ad-client="ca-pub-8239493855484450"
       data-ad-slot="4566419322"
       data-ad-format="auto"
       data-full-width-responsive="true"
-    />
+    ></ins>
   );
 };
 
